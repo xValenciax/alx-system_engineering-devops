24bfe21305a9381bef52de6b250a1321a163c5e5
Selim <mamo31st@gmail.com>
2024-06-29 05:00:46 +0300

    updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,30 @@
-#!/usr/bin/pup
-# Using Puppet, install flask from pip3
+# Puppet Manifest: 1-install_a_package.pp
 
-package {'Werkzeug':
-ensure  => '2.1.1'
+# Ensure Python and pip are installed
+package { 'python3-pip':
+  ensure => installed,
 }
 
-package { 'flask':
+package { 'python3':
+  ensure => installed,
+}
+
+# Install Flask 2.1.0 using pip
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['Werkzeug']
+  provider => 'pip',
+  require  => [
+    Package['python3-pip'],  # Ensure python3-pip is installed first
+    Package['python3'],      # Ensure python3 is installed first
+  ],
+}
+
+# Install Werkzeug 2.1.1 using pip
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => [
+    Package['python3-pip'],  # Ensure python3-pip is installed first
+    Package['python3'],      # Ensure python3 is installed first
+  ],
 }
