2a6760f216d5f0a919ca77ec234688eadb8627a4
Selim <mamo31st@gmail.com>
2024-06-29 05:06:15 +0300

    updates

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -18,13 +18,3 @@ package { 'flask':
     Package['python3'],      # Ensure python3 is installed first
   ],
 }
-
-# Install Werkzeug 2.1.1 using pip
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip',
-  require  => [
-    Package['python3-pip'],  # Ensure python3-pip is installed first
-    Package['python3'],      # Ensure python3 is installed first
-  ],
-}
