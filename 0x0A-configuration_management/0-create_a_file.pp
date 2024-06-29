a5a83002bd1427f962a982c5745166541bff55ff
Selim <mamo31st@gmail.com>
2024-06-29 04:09:21 +0300

    updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-file {'/tmp/school':
-      content => 'I love Puppet',
+# creates new file named school in /tmp dir
+file {'school':
+      ensure  => 'present',
       mode    => '0744',
+      path    => '/tmp/school',
+      content => 'I love Puppet',
       group   => 'www-data',
       owner   => 'www-data'
 }
