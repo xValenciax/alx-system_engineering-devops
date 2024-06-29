d1f61bdbf48ca94ba4e007f1328bf897a916f56a
Selim <mamo31st@gmail.com>
2024-06-29 04:51:30 +0300

    updates

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # kill a process named killmenow
 
-exec { 'terminate_process':
-  command     => '/usr/bin/pkill -9 process_name',
-  refreshonly => true,
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
